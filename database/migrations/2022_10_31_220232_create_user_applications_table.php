8d5f2fdbaf49ccd1504bd54a11827c25d5a00409
Daniel Valente <danieelvaraujo@hotmail.com>
2022-10-31 19:03:33 -0300

    chore: removed curriculums table and created user-applications instead

--- database/migrations/2022_10_31_220232_create_user_applications_table.php
+++ database/migrations/2022_10_31_220232_create_user_applications_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('curriculums', function (Blueprint $table) {
+        Schema::create('user_applications', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email');
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('curriculums');
+        Schema::dropIfExists('user_applications');
     }
 };
