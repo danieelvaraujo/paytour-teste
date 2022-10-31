9efc29c1533714687badcac41e9ddb3a8c4ca398
Daniel Valente <danieelvaraujo@hotmail.com>
2022-10-31 14:28:13 -0300

    chore: created send function

--- app/Http/Controllers/CurriculumController.php
+++ app/Http/Controllers/CurriculumController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class CurriculumController extends Controller
 {
-    //
+    public function send(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'bail|required|min:3',
+            'email' => 'bail|required|email',
+            'telephone' => 'required',
+            'desired_job_title' => 'bail|required|string',
+            'scholarity' => 'required',
+            'observations' => 'string',
+        ]);
+
+        $curriculum = Curriculum::create($data);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Curriculum created sucessfully',
+            'data' => $curriculum
+        ], Response::HTTP_OK);
+    }
 }
