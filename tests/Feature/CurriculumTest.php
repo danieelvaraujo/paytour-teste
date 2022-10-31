09a972eccb1c210ee23ef0c4ed6b7945e0d681bb
Daniel Valente <danieelvaraujo@hotmail.com>
2022-10-31 17:15:33 -0300

    chore: added test case to upload file

--- tests/Feature/CurriculumTest.php
+++ tests/Feature/CurriculumTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class CurriculumTest extends TestCase
@@ -27,6 +29,29 @@ class CurriculumTest extends TestCase
         $response = $this->post('/send-curriculum', $data);
 
         $response->assertOk();
+        $this->assertDatabaseHas('curriculums', [
+            'email' => $data['email'],
+        ]);
+    }
+
+    public function test_user_curriculum_upload()
+    {
+        Storage::fake('curriculums');
+
+        $data = [
+            'name' => 'Usuario Testador',
+            'email' => 'teste@email.com',
+            'telephone' => '84987654321',
+            'desired_job_title' => 'Desenvolvedor backend',
+            'scholarity' => 'Ensino superior completo',
+            'observations' => 'Campo opcional.',
+            'file' => UploadedFile::fake()->create('usuario-tostador-cv', 1, '.doc')
+        ];
+
+        $response = $this->post('/send-curriculum', $data);
+
+        $response->assertOk();
+        Storage::disk('curriculums')->assertExists('usuario-tostador-cv.doc');
         $this->assertDatabaseHas('curriculums', [
             'email' => $data['email'],
         ]);
