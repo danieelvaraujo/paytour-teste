90d22948f7d2fd7a01ad730f9f3024b38f093d1b
Daniel Valente <danieelvaraujo@hotmail.com>
2022-11-01 11:18:11 -0300

    chore: rearranged routes

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <div class="py-12">
+        <h1>Bem vindo</h1>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserApplicationController;
+use App\Models\UserApplication;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,13 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
+Route::get('/send-application', [UserApplication::class, 'show'])->name('send-application');
 Route::post('/send-application', [UserApplicationController::class, 'send']);
 Route::post('/upload-curriculum', [UserApplicationController::class, 'upload']);
 
