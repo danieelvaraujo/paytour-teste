7e431b13118a0dd7996f1e8e01419e1873dd64a9
Daniel Valente <danieelvaraujo@hotmail.com>
2022-11-01 11:25:27 -0300

    chore: created test case for sending application with curriculum

--- tests/Feature/UserApplicationTest.php
+++ tests/Feature/UserApplicationTest.php
@@ -4,13 +4,13 @@ namespace Tests\Feature;
 
 use App\Models\UserApplication;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class UserApplicationTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
@@ -55,6 +55,29 @@ class UserApplicationTest extends TestCase
 
         $response->assertOk();
         Storage::disk('curriculums')->assertExists('usuario-tostador-cv.pdf');
+        $this->assertDatabaseHas('curriculums', [
+            'filename' => $filename,
+        ]);
+    }
+
+    public function test_user_can_send_application_with_curriculum_attached()
+    {
+        $filename = 'usuario-tostador-cv.pdf';
+        $response = $this->post('/send-application', [
+            'name' => 'Usuario Testador',
+            'email' => 'teste@email.com',
+            'telephone' => '84987654321',
+            'desired_job_title' => 'Desenvolvedor backend',
+            'scholarity' => 'Ensino superior completo',
+            'observations' => 'Campo opcional.',
+            'file' => UploadedFile::fake()->create($filename, 1024),
+        ]);
+
+        $response->assertOk();
+        Storage::disk('curriculums')->assertExists('usuario-tostador-cv.pdf');
+        $this->assertDatabaseHas('user_applications', [
+            'email' => "teste@email.com",
+        ]);
         $this->assertDatabaseHas('curriculums', [
             'filename' => $filename,
         ]);
