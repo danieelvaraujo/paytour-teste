aabcb4241d77ad2ebfef63480fe6f5927712f5ef
Daniel Valente <danieelvaraujo@hotmail.com>
2022-10-31 23:00:21 -0300

    chore: created curriculums migration

---
+++ database/migrations/2022_11_01_015536_create_curriculums_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curriculums', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('filename');
+            $table->integer('applicant_id')->unsigned();
+
+            $table->foreign('applicant_id')->references('id')->on('user_applications');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curriculums');
+    }
+};
