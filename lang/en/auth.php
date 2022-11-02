b4c6079a88db5b7037fbdbe6d0a5289210adc1b4
Daniel Valente <danieelvaraujo@hotmail.com>
2022-11-02 15:07:54 -0300

    chore: translated form validations

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Credenciais inválidas.',
+    'password' => 'Senha incorreta.',
+    'throttle' => 'Foram feitas muitas tentativas. Por favor tente novamente em :seconds segundos.',
 
 ];
--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Sua senha foi resetada!',
+    'sent' => 'Nós te enviamos por email um link para resetar a senha!',
+    'throttled' => 'Favor aguardar antes de tentar novamente.',
+    'token' => 'O token de resetar a senha é inválido.',
+    'user' => "Não encontramos um usuário com este email.",
 
 ];
