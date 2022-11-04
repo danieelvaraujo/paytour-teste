a4c0b6887d207a392ffa5b7ce75fbd5766508e5d
Daniel Valente <danieelvaraujo@hotmail.com>
2022-11-04 00:08:43 -0300

    chore: updated user application test cases

--- tests/Feature/UserApplicationTest.php
+++ tests/Feature/UserApplicationTest.php
@@ -52,109 +52,109 @@ class UserApplicationTest extends TestCase
         ]);
     }
 
-    // public function test_user_can_upload_a_curriculum()
-    // {
-    //     $filename = 'usuario-testador-cv.pdf';
-    //     $application = UserApplication::create($this->userApplicationTest);
+    public function test_user_can_upload_a_curriculum()
+    {
+        $filename = 'usuario-testador-cv.pdf';
+        $application = UserApplication::create($this->userApplicationTest);
 
-    //     $response = $this->post('/upload-curriculum', [
-    //         'name' => 'Usuario Testador',
-    //         'file' => UploadedFile::fake()->create($filename, 1024),
-    //         'applicant_id' => $application->id
-    //     ]);
+        $response = $this->post('/upload-curriculum', [
+            'name' => 'Usuario Testador',
+            'file' => UploadedFile::fake()->create($filename, 1024),
+            'applicant_id' => $application->id
+        ]);
 
-    //     $response->assertOk();
-    //     Storage::disk('curriculums')->assertExists('usuario-testador-cv.pdf');
-    //     $this->assertDatabaseHas('curriculums', [
-    //         'filename' => $filename,
-    //     ]);
-    // }
+        $response->assertOk();
+        Storage::disk('curriculums')->assertExists('usuario-testador-cv.pdf');
+        $this->assertDatabaseHas('curriculums', [
+            'filename' => $filename,
+        ]);
+    }
 
-    // public function test_user_cant_upload_a_curriculum_with_any_format()
-    // {
-    //     // Accepted formats: doc, docx, pdf
+    public function test_user_cant_upload_a_curriculum_with_any_format()
+    {
+        // Accepted formats: doc, docx, pdf
 
-    //     $filename = 'usuario-testador-cv.jpg';
-    //     $application = UserApplication::create($this->userApplicationTest);
+        $filename = 'usuario-testador-cv.jpg';
+        $application = UserApplication::create($this->userApplicationTest);
 
-    //     $response = $this->post('/upload-curriculum', [
-    //         'name' => 'Usuario Testador',
-    //         'file' => UploadedFile::fake()->create($filename, 1024),
-    //         'applicant_id' => $application->id
-    //     ]);
+        $response = $this->post('/upload-curriculum', [
+            'name' => 'Usuario Testador',
+            'file' => UploadedFile::fake()->create($filename, 1024),
+            'applicant_id' => $application->id
+        ]);
 
-    //     $response->assertSessionHasErrors();
-    // }
+        $response->assertSessionHasErrors();
+    }
 
-    // public function test_user_cant_upload_a_curriculum_with_size_greater_than_1mb()
-    // {
-    //     // Accepted formats: doc, docx, pdf
+    public function test_user_cant_upload_a_curriculum_with_size_greater_than_1mb()
+    {
+        // Accepted formats: doc, docx, pdf
 
-    //     $filename = 'usuario-testador-cv.doc';
-    //     $application = UserApplication::create($this->userApplicationTest);
+        $filename = 'usuario-testador-cv.doc';
+        $application = UserApplication::create($this->userApplicationTest);
 
-    //     $response = $this->post('/upload-curriculum', [
-    //         'name' => 'Usuario Testador',
-    //         'file' => UploadedFile::fake()->create($filename, 2048),
-    //         'applicant_id' => $application->id
-    //     ]);
+        $response = $this->post('/upload-curriculum', [
+            'name' => 'Usuario Testador',
+            'file' => UploadedFile::fake()->create($filename, 2048),
+            'applicant_id' => $application->id
+        ]);
 
-    //     $response->assertSessionHasErrors();
-    // }
+        $response->assertSessionHasErrors();
+    }
 
-    // public function test_user_can_send_application_with_curriculum_attached()
-    // {
-    //     $filename = 'usuario-testador-cv.pdf';
-    //     $application = $this->userApplicationTest;
-    //     $application['file'] = UploadedFile::fake()->create($filename, 1024);
+    public function test_user_can_send_application_with_curriculum_attached()
+    {
+        $filename = 'usuario-testador-cv.pdf';
+        $application = $this->userApplicationTest;
+        $application['file'] = UploadedFile::fake()->create($filename, 1024);
 
-    //     $response = $this->post('/send-application', $application);
+        $response = $this->post('/send-application', $application);
 
-    //     $response->assertOk();
-    //     Storage::disk('curriculums')->assertExists('usuario-testador-cv.pdf');
-    //     $this->assertDatabaseHas('user_applications', [
-    //         'email' => "teste@email.com",
-    //     ]);
-    //     $this->assertDatabaseHas('curriculums', [
-    //         'filename' => $filename,
-    //     ]);
-    // }
+        $response->assertOk();
+        Storage::disk('curriculums')->assertExists('usuario-testador-cv.pdf');
+        $this->assertDatabaseHas('user_applications', [
+            'email' => "teste@email.com",
+        ]);
+        $this->assertDatabaseHas('curriculums', [
+            'filename' => $filename,
+        ]);
+    }
 
-    // public function test_user_application_saves_ip_from_applicant()
-    // {
-    //     $this->withServerVariables(['REMOTE_ADDR' => '10.1.0.1']);
-    //     $filename = 'usuario-testador-cv.pdf';
-    //     $application = $this->userApplicationTest;
-    //     $application['file'] = UploadedFile::fake()->create($filename, 1024);
+    public function test_user_application_saves_ip_from_applicant()
+    {
+        $this->withServerVariables(['REMOTE_ADDR' => '10.1.0.1']);
+        $filename = 'usuario-testador-cv.pdf';
+        $application = $this->userApplicationTest;
+        $application['file'] = UploadedFile::fake()->create($filename, 1024);
 
-    //     $response = $this->post('/send-application', $application);
+        $response = $this->post('/send-application', $application);
 
-    //     $response->assertOk();
-    //     $this->assertDatabaseHas('user_applications', [
-    //         'ip_address' => '10.1.0.1',
-    //     ]);
-    // }
+        $response->assertOk();
+        $this->assertDatabaseHas('user_applications', [
+            'ip_address' => '10.1.0.1',
+        ]);
+    }
 
-    // public function test_user_application_saves_user_id_from_applicant()
-    // {
-    //     $application = UserApplication::create([
-    //         'name' => 'Usuario Testador',
-    //         'email' => 'teste@email.com',
-    //         'telephone' => '84987654321',
-    //         'desired_job_title' => 'Desenvolvedor backend',
-    //         'scholarity' => 'Ensino superior completo',
-    //         'observations' => 'Campo opcional.',
-    //         'ip_address' => '10.0.0.1',
-    //         'user_id' => $user->id
-    //     ]);
+    public function test_user_application_saves_user_id_from_applicant()
+    {
+        $application = UserApplication::create([
+            'name' => 'Usuario Testador',
+            'email' => 'teste@email.com',
+            'telephone' => '84987654321',
+            'desired_job_title' => 'Desenvolvedor backend',
+            'scholarity' => 'Ensino superior completo',
+            'observations' => 'Campo opcional.',
+            'ip_address' => '10.0.0.1',
+            'user_id' => $this->testUser->id
+        ]);
 
-    //     $response = $this->post('/send-application', $application->toArray());
+        $response = $this->post('/send-application', $application->toArray());
 
-    //     $response->assertOk();
-    //     $this->assertDatabaseHas('user_applications', [
-    //         'email' => $application->email,
-    //     ]);
-    // }
+        $response->assertOk();
+        $this->assertDatabaseHas('user_applications', [
+            'user_id' => $application->user_id,
+        ]);
+    }
 
     // public function test_sends_an_email_to_user_if_application_is_successful()
     // {
