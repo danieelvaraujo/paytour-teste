9c6e9c7c05afb2b14e159843052c8c1c1db11dff
Daniel Valente <danieelvaraujo@hotmail.com>
2022-11-03 22:42:40 -0300

    chore: dashboard now depends on the parameter id

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserApplication;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    public UserApplication $userApplication;
+
+    public function __construct($id)
+    {
+        $this->userApplication = UserApplication::where('user_id', $id)->first();
+    }
+
     public function show()
     {
-        return view('dashboard');
+        return view('dashboard', compact($this->userApplication));
     }
 
 }
