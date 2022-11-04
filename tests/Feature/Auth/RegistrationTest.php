79369daba901029d98191257cadec7c2ec7dd9a6
Daniel Valente <danieelvaraujo@hotmail.com>
2022-11-04 00:42:57 -0300

    chore: test case for redirect user when registered

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Auth;
 
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
 class RegistrationTest extends TestCase
@@ -19,6 +20,18 @@ class RegistrationTest extends TestCase
 
     public function test_new_users_can_register()
     {
+        $this->post('/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $this->assertAuthenticated();
+    }
+
+    public function test_new_users_are_redirected_when_register_is_sucessful()
+    {
         $response = $this->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
@@ -27,6 +40,6 @@ class RegistrationTest extends TestCase
         ]);
 
         $this->assertAuthenticated();
-        $response->assertRedirect(RouteServiceProvider::HOME);
+        $response->assertRedirect(RouteServiceProvider::HOME . '/' . Auth::user()->id);
     }
 }
