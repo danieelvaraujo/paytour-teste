509ebb022cd3af6bfdf77aa1d36414aed81bc6f7
Daniel Valente <danieelvaraujo@hotmail.com>
2022-11-04 13:15:22 -0300

    chore: switched function between controllers

--- app/Http/Controllers/UpdateApplicationController.php
+++ app/Http/Controllers/UpdateApplicationController.php
@@ -2,12 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserApplication;
 use Illuminate\Http\Request;
 
+use Symfony\Component\HttpFoundation\Response;
 class UpdateApplicationController extends Controller
 {
     public function show()
     {
         return view('update-application');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'min:3',
+            'telephone' => 'integer',
+            'desired_job_title' => 'string'
+        ]);
+
+        $updated = UserApplication::find($id);
+
+        $updated->name = $request->name ?? $updated->name;
+        $updated->email = $updated->email;
+        $updated->telephone = $request->telephone ?? $updated->telephone;
+        $updated->desired_job_title = $request->desired_job_title ?? $updated->desired_job_title;
+        $updated->scholarity = $request->scholarity ?? $updated->scholarity;
+        $updated->observations = $request->observations  ?? $updated->observations;
+        $updated->ip_address = $updated->ip_address;
+        $updated->user_id = $updated->user_id;
+
+        $updated->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User application updated.',
+            'data' => $updated
+        ], Response::HTTP_OK);
     }
 }
--- app/Http/Controllers/UserApplicationController.php
+++ app/Http/Controllers/UserApplicationController.php
@@ -82,32 +82,4 @@ class UserApplicationController extends Controller
             'data' => $curriculum
         ], Response::HTTP_OK);
     }
-
-    public function update(Request $request, $id)
-    {
-        $request->validate([
-            'name' => 'min:3',
-            'telephone' => 'integer',
-            'desired_job_title' => 'string'
-        ]);
-
-        $updated = UserApplication::find($id);
-
-        $updated->name = $request->name ?? $updated->name;
-        $updated->email = $updated->email;
-        $updated->telephone = $request->telephone ?? $updated->telephone;
-        $updated->desired_job_title = $request->desired_job_title ?? $updated->desired_job_title;
-        $updated->scholarity = $request->scholarity ?? $updated->scholarity;
-        $updated->observations = $request->observations  ?? $updated->observations;
-        $updated->ip_address = $updated->ip_address;
-        $updated->user_id = $updated->user_id;
-
-        $updated->save();
-
-        return response()->json([
-            'success' => true,
-            'message' => 'User application updated.',
-            'data' => $updated
-        ], Response::HTTP_OK);
-    }
 }
