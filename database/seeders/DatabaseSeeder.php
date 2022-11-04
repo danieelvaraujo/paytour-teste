5fe26a3802e5e04c8a1ee032a8ef652a667e171d
Daniel Valente <danieelvaraujo@hotmail.com>
2022-11-04 15:07:16 -0300

    feat: using scholarity as table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,32 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\Scholarity::factory()->create([
+            [
+                'title' => 'Ensino fundamental incompleto',
+                'value' => 'fundamental_incompleto'
+            ],
+            [
+                'title' => 'Ensino fundamental completo',
+                'value' => 'fundamental_completo'
+            ],
+            [
+                'title' => 'Ensino médio incompleto',
+                'value' => 'medio_incompleto'
+            ],
+            [
+                'title' => 'Ensino médio completo',
+                'value' => 'medio_completo'
+            ],
+            [
+                'title' => 'Ensino superior incompleto',
+                'value' => 'superio_completo'
+            ],
+            [
+                'title' => 'Ensino superior completo',
+                'value' => 'superior_completo'
+            ],
+        ]);
     }
 }
