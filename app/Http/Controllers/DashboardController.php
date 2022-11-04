a0a0d147d8857187e7045cffe6743556428bdfa6
Daniel Valente <danieelvaraujo@hotmail.com>
2022-11-04 00:31:47 -0300

    chore: changed construct function to get only id in dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,18 +8,17 @@ use Illuminate\Support\Facades\Route;
 
 class DashboardController extends Controller
 {
-    public UserApplication $userApplication;
+    public $id;
 
     public function __construct()
     {
-        $id = Route::current()->parameter('id');
-        $this->userApplication = UserApplication::where('user_id', $id)
-            ->first();
+        $this->id = Route::current()->parameter('id');
     }
 
     public function show()
     {
-        $userApplication = $this->userApplication;
+        $userApplication = UserApplication::where('user_id', $this->id)
+            ->first();
 
         return view('dashboard', compact('userApplication'));
     }
