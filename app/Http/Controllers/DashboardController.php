31fb6a13d7a9edfa74efb8f1aee9bffaeb998620
Daniel Valente <danieelvaraujo@hotmail.com>
2022-11-03 22:51:52 -0300

    chore: reworked construct to get id from parameter

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\UserApplication;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class DashboardController extends Controller
 {
     public UserApplication $userApplication;
 
-    public function __construct($id)
+    public function __construct()
     {
+        $id = Route::current()->parameter('id');
         $this->userApplication = UserApplication::where('user_id', $id)->first();
     }
 
-    public function show()
+    public function show($id)
     {
         return view('dashboard', compact($this->userApplication));
     }
