b84aaa3f9948cf12fb86005729383630205bd566
Daniel Valente <danieelvaraujo@hotmail.com>
2022-11-05 21:10:33 -0300

    feat: added redirect back to cancel button in send application

--- resources/views/send-application.blade.php
+++ resources/views/send-application.blade.php
@@ -232,6 +232,7 @@
         <div class="pt-5">
             <div class="flex justify-end">
             <button
+                onclick="window.location.href='{{ url()->previous() }}'"
                 type="button"
                 class="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
